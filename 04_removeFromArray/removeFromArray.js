58e313051c6e25ec006af36fb70056b2f2f8d0ff
clock783 <aodunlade3@gmail.com>
2023-05-02 07:17:29 -0400

    Create removeFromArray. Passed 1st test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    // console.log(argunments.length);
+    let newArray = [];
+    for (i=0; i<args.length; i++){
+        let n = args[i];
+        for (j=0; j<array.length; j++){
+            if (!(n === array[j])){
+                newArray.push(array[j]);
+            }
+        }
+    }
+return newArray;
 };
 
 // Do not edit below this line
