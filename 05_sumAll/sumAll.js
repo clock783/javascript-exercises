e1093cdc94769e7c45cf0cedd67723eebdcd0c43
clock783 <aodunlade3@gmail.com>
2023-05-04 07:44:57 -0400

    Partial Solution for sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,16 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    //check types of inputs and confirm they are positive integers
+    let args = [a,b];
+    args.forEach((item) =>{
+        if (!Number.isInteger(item) && (item < 0)){
+            return 'ERROR';
+        }
+    })
+    //grab upper and lower numbers
+    let low = Math.min(a,b);
+    let high = Math.max(a,b);
+    //use math instead of loops
+    return (high + low)*((high-low+1)/2);
 
 };
 
--- 05_sumAll/sumAll.spec.js
+++ 05_sumAll/sumAll.spec.js
@@ -4,10 +4,10 @@ describe('sumAll', () => {
   test('sums numbers within the range', () => {
     expect(sumAll(1, 4)).toEqual(10);
   });
-  test.skip('works with large numbers', () => {
+  test('works with large numbers', () => {
     expect(sumAll(1, 4000)).toEqual(8002000);
   });
-  test.skip('works with larger number first', () => {
+  test('works with larger number first', () => {
     expect(sumAll(123, 1)).toEqual(7626);
   });
   test.skip('returns ERROR with negative numbers', () => {
@@ -16,7 +16,7 @@ describe('sumAll', () => {
   test.skip('returns ERROR with non-number parameters', () => {
     expect(sumAll(10, "90")).toEqual('ERROR');
   });
-  test.skip('returns ERROR with non-number parameters', () => {
+  test('returns ERROR with non-number parameters', () => {
     expect(sumAll(10, [90, 1])).toEqual('ERROR');
   });
 });
