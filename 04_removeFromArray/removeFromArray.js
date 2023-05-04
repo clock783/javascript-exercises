974683bfc89281eab9628eb6d05b564cb0ffb158
clock783 <aodunlade3@gmail.com>
2023-05-04 06:38:05 -0400

    Complete removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,13 +1,21 @@
 const removeFromArray = function(array, ...args) {
-    // console.log(argunments.length);
+    //this function checks if args are in the array and removes the args.
+    //for every arg, if the arg is not in the array, it adds it to a new array.
+    //after completing one iteration of checks for an arg, the next arg is only
+    //checked against the new array, not the inital array. the new array is re-initialized
+    //at the begining of the arg check.
+
+    let copyArray = array;
     let newArray = [];
     for (i=0; i<args.length; i++){
         let n = args[i];
-        for (j=0; j<array.length; j++){
-            if (!(n === array[j])){
-                newArray.push(array[j]);
+        newArray = []; 
+        for (j=0; j<copyArray.length; j++){
+            if (!(n === copyArray[j])){
+                newArray.push(copyArray[j]);
             }
         }
+        copyArray = newArray;
     }
 return newArray;
 };
