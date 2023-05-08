ea627dbef836f6fc4086745f1a5aff5c67c9d8e1
clock783 <aodunlade3@gmail.com>
2023-05-08 06:57:20 -0400

    Complete sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,11 +1,31 @@
 const sumAll = function(a,b) {
     //check types of inputs and confirm they are positive integers
     let args = [a,b];
-    args.forEach((item) =>{
-        if (!Number.isInteger(item) && (item < 0)){
+    //console.log(args);
+    
+    for(i=0; i<args.length; i++){
+        if (Number.isInteger(args[i]) === false){
+            //console.log('not a number');
+            return 'ERROR';
+        }    
+        if (args[i] < 0){
+            //console.log('less than zero')
             return 'ERROR';
         }
-    })
+    }
+    //.forEach does not work in this instance because there is no way to
+    //stop or break a forEach loop
+    // args.forEach((item) =>{
+    //     if (Number.isInteger(item) === false){
+    //         console.log('not a number');
+    //         return 'ERROR';
+    //     }
+    //     if (item < 0){
+    //         console.log('less than zero')
+    //         return 'ERROR';
+    //     }
+    // })
+    
     //grab upper and lower numbers
     let low = Math.min(a,b);
     let high = Math.max(a,b);
