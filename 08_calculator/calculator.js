87c90d5285adb1738a9ce5e092b1cc09b1c6172d
clock783 <aodunlade3@gmail.com>
2023-11-26 16:17:16 -0500

    Complete 08 calculator exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,34 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum,current)=>sum + current,0);
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  return arr.reduce((product,current)=>product * current,1);
 
 };
 
-const power = function() {
-	
+const power = function(a,n) {
+	return a ** n;
 };
 
-const factorial = function() {
+const factorial = function(n) {
+  if(n <= 1){
+    return 1;
+  } else{
+    let product = 1;
+    for(i = n; i>1; i--){
+      product *= i;
+    }
+    return product;
+  }
 	
 };
 
