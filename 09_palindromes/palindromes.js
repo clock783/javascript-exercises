e97716d257ffae662e4d4b8535a6bf33a6221dbb
clock783 <aodunlade3@gmail.com>
2023-11-28 07:51:15 -0500

    Finish palindrome and start fibonacci

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,38 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    //convert string to lowercase
+    let lowerStrArr = str.toLowerCase();
+    let strList = lowerStrArr.split('');
+    
+    //strip characters that are not letters or numbers
+    let reg = new RegExp(/[a-z0-9]/);
+    let stripped = strList.reduce((currTot, currVal)=>{
+        if(reg.test(currVal)){
+            currTot.push(currVal);
+        }
+        return currTot;
+    },[]);
 
+    //loop through and check equality of start and end indices. increase/reduce indices in every loop
+    //  only check half the length rounded down. if length is odd, it will skip the middle.
+    let stillPal = true;
+    let i = 0;
+    let right = stripped.length - 1;
+    while(stillPal && (i+1)<=(stripped.length/2)){
+        //3 items. i = 0, length is 3. test is 1 <= 1: passes!
+        //4 items. i = 0, length is 4. test is 1<= 2: passes!
+        if(stripped[i]!==stripped[right]){
+            return false;
+        }
+        i++;
+        right--;
+    }
+    return true
+    // console.log(strList);
+    // console.log(stripped);
 };
+
+// console.log(palindromes("A cat in a h-at!"));
+// console.log(palindromes('Animal loots foliated detail of stool lamina.'));
 
 // Do not edit below this line
 module.exports = palindromes;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let one = 0;
+    let next = 1;
 
+    for(i=0; i<=n; i++){
+        
+    }
 };
 
 // Do not edit below this line
