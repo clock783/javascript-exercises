61eb2a2f2bee2189be3d290a35553e211733751b
clock783 <aodunlade3@gmail.com>
2024-01-02 07:27:16 -0500

    Finish findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,32 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let sorted = people.sort((a,b)=>{
+        let current = new Date().getFullYear();
+        let first = (a.yearOfDeath || current) - a.yearOfBirth;
+        let second = (b.yearOfDeath ||current) - b.yearOfBirth;
+        return first > second ? -1 : 1;
+    });
+    return sorted[0];
 };
+
+// const people = [
+//     {
+//       name: "Carly",
+//       yearOfBirth: 1942,
+//       yearOfDeath: 1970,
+//     },
+//     {
+//       name: "Ray",
+//       yearOfBirth: 1962,
+//       yearOfDeath: 2011,
+//     },
+//     {
+//       name: "Jane",
+//       yearOfBirth: 1912,
+//       yearOfDeath: 1941,
+//     },
+//   ]
+
+// console.log(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
